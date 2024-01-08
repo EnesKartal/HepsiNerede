859dbb8ca658d9bfeba31e15e562558e642ec660
EnesKartal <eneskartal17@gmail.com>
2024-01-09 00:10:25 +0300

    CampaignService more test added

--- HepsiNerede.Tests/CampaignServiceTests.cs
+++ HepsiNerede.Tests/CampaignServiceTests.cs
@@ -1,5 +1,6 @@
 using HepsiNerede.Data.Entities;
 using HepsiNerede.Data.Repositories;
+using HepsiNerede.Models.DTO.Campaign.CreateCampaign;
 using HepsiNerede.Services;
 using Moq;
 
@@ -8,12 +9,13 @@ namespace HepsiNerede.Tests
     public class CampaignServiceTests
     {
         [Fact]
-        public void AddCampaign_ShouldAddCampaignToRepository()
+        public void GetCampaignByName_ShouldReturnCampaign_WhenCampaignExists()
         {
+            // Arrange
             var campaignRepositoryMock = new Mock<ICampaignRepository>();
             var campaignService = new CampaignService(campaignRepositoryMock.Object);
 
-            var campaign = new Campaign
+            var expectedCampaign = new Campaign
             {
                 Name = "SummerSale",
                 ProductCode = "P001",
@@ -23,7 +25,66 @@ namespace HepsiNerede.Tests
                 Id = 1
             };
 
-            campaignService.CreateCampaign(campaign.Name, campaign.ProductCode, campaign.PriceManipulationLimit, campaign.TargetSalesCount, campaign.Duration);
+            campaignRepositoryMock.Setup(repo => repo.GetCampaignByName("SummerSale")).Returns(expectedCampaign);
+
+            // Act
+            var actualCampaign = campaignService.GetCampaignByName("SummerSale");
+
+            // Assert
+            Assert.NotNull(actualCampaign);
+            Assert.Equal(expectedCampaign.Name, actualCampaign.Name);
+            Assert.Equal(expectedCampaign.ProductCode, actualCampaign.ProductCode);
+            Assert.Equal(expectedCampaign.Duration, actualCampaign.Duration);
+            Assert.Equal(expectedCampaign.PriceManipulationLimit, actualCampaign.PriceManipulationLimit);
+            Assert.Equal(expectedCampaign.TargetSalesCount, actualCampaign.TargetSalesCount);
+
+            campaignRepositoryMock.Verify(repo => repo.GetCampaignByName("SummerSale"), Times.Once);
+        }
+
+        [Fact]
+        public void GetCampaignByName_ShouldReturnNull_WhenCampaignDoesNotExist()
+        {
+            // Arrange
+            var campaignRepositoryMock = new Mock<ICampaignRepository>();
+            var campaignService = new CampaignService(campaignRepositoryMock.Object);
+
+            // Act
+            var actualCampaign = campaignService.GetCampaignByName("NonExistingCampaign");
+
+            // Assert
+            Assert.Null(actualCampaign);
+
+            campaignRepositoryMock.Verify(repo => repo.GetCampaignByName("NonExistingCampaign"), Times.Once);
+        }
+
+        [Fact]
+        public void CreateCampaign_ShouldCreateCampaignAndReturnIt()
+        {
+            // Arrange
+            var campaignRepositoryMock = new Mock<ICampaignRepository>();
+            var campaignService = new CampaignService(campaignRepositoryMock.Object);
+
+            var createCampaignRequest = new CreateCampaignRequestDTO
+            {
+                Name = "WinterSale",
+                ProductCode = "P002",
+                Duration = 48,
+                PMLimit = 15,
+                TSCount = 75
+            };
+
+            campaignRepositoryMock.Setup(repo => repo.CreateCampaign(It.IsAny<Campaign>())).Returns<Campaign>(c => c);
+
+            // Act
+            var createdCampaign = campaignService.CreateCampaign(createCampaignRequest);
+
+            // Assert
+            Assert.NotNull(createdCampaign);
+            Assert.Equal(createCampaignRequest.Name, createdCampaign.Name);
+            Assert.Equal(createCampaignRequest.ProductCode, createdCampaign.ProductCode);
+            Assert.Equal(createCampaignRequest.Duration, createdCampaign.Duration);
+            Assert.Equal(createCampaignRequest.PMLimit, createdCampaign.PriceManipulationLimit);
+            Assert.Equal(createCampaignRequest.TSCount, createdCampaign.TargetSalesCount);
 
             campaignRepositoryMock.Verify(repo => repo.CreateCampaign(It.IsAny<Campaign>()), Times.Once);
         }
