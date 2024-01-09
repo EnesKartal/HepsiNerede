82e95a7c0aa948c3df23c476934b69e8378f4a1c
EnesKartal <eneskartal17@gmail.com>
2024-01-09 19:44:02 +0300

    Add ErrorHandlingMiddleware and Swagger configuration

---
+++ HepsiNerede.WebApp/Core/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using HepsiNerede.WebApp.Common;
+using System.Net;
+using System.Text.Json;
+
+namespace HepsiNerede.WebApp.Core.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var response = new ApiResponse
+                {
+                    Success = false,
+                    Message = $"Internal Server Error: {ex.Message}"
+                };
+
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var json = JsonSerializer.Serialize(response);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+    public static class ErrorHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseErrorHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ErrorHandlingMiddleware>();
+        }
+    }
+}
--- HepsiNerede.WebApp/Program.cs
+++ HepsiNerede.WebApp/Program.cs
@@ -1,7 +1,7 @@
 using HepsiNerede.Data;
 using HepsiNerede.Data.Repositories;
 using HepsiNerede.Services;
-using Microsoft.AspNetCore.Builder;
+using HepsiNerede.WebApp.Core.Middlewares;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +9,6 @@ var configuration = builder.Configuration;
 
 builder.Services.AddDbContext<HepsiNeredeDBContext>(options =>
 {
-    //get configuration from appsettings.json
     var hepsiNeredeConnectionString = configuration.GetConnectionString("HepsiNeredeDBContext");
     options.UseSqlite(hepsiNeredeConnectionString);
 });
@@ -25,14 +24,23 @@ builder.Services.AddScoped<IOrderService, OrderService>();
 
 builder.Services.AddSingleton<ITimeSimulationService, TimeSimulationService>();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "HepsiNerede API", Version = "v1", });
+});
 
 
 builder.Services.AddControllers();
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "HepsiNerede API V1");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseErrorHandlingMiddleware();
 
 using (var scope = app.Services.CreateScope())
 {
