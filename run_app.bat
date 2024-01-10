e75a506f424ac051752f694e7519abf5f7a21002
Enes Kartal <43950508+EnesKartal@users.noreply.github.com>
2024-01-10 18:29:08 +0300

    Update run_app.bat

--- run_app.bat
+++ run_app.bat
@@ -3,6 +3,14 @@
 echo "Restoring dependencies..."
 dotnet restore
 
+if %ERRORLEVEL% neq 0 (
+    echo "Error restoring dependencies. Exiting."
+    PAUSE
+    exit /b 1
+)
+
 echo "Launching the application..."
 start chrome http://localhost:5041
 dotnet run --project ./src/HepsiNerede.WebApp/HepsiNerede.WebApp.csproj
+
+PAUSE
